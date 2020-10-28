4dbdbc33ab77bbfa2e653b1fc581f392c2f331ab
rebeccasc <65017227+rebeccasc@users.noreply.github.com>
2020-10-28 19:47:55 +0100

    Reset AutoCalibrationAction after each calibration to avoid errors

--- src/org/openstreetmap/josm/plugins/piclayer/actions/transform/autocalibrate/AutoCalibrateHandler.java
+++ src/org/openstreetmap/josm/plugins/piclayer/actions/transform/autocalibrate/AutoCalibrateHandler.java
@@ -224,6 +224,7 @@ public class AutoCalibrateHandler {
                 currentPicLayer.getTransformer().resetCalibration();
                 currentPicLayer.invalidate();
             }
+            reset();
         }
 
         /**
